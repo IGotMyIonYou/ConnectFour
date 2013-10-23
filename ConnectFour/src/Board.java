7a77350503270e74b2a258775702dd2c53a29329
IGotMyIonYou <IonicZen@gmail.com>
2013-10-23 18:19:21 -0500

    Minor changes

--- ConnectFour/src/Board.java
+++ ConnectFour/src/Board.java
@@ -4,11 +4,10 @@ import javax.swing.JPanel;
 
 public class Board extends JPanel 
 {
-   // display various lines, rectangles and ovals
    public void paintComponent( Graphics g )
    {
 	  int i, j, startX = 205, startY = 105, radius = 60;
-      super.paintComponent( g ); // call superclass's paint method
+      super.paintComponent( g );
 
       this.setBackground( Color.WHITE);
 
@@ -16,18 +15,10 @@ public class Board extends JPanel
       g.fillRect(200, 100, 480, 410);
 
       g.setColor( Color.BLACK );
-      //g.fillOval(startX, startY, radius, radius);
       for(i=0; i<6; i++){
     	  for(j=0; j<7; j++){
     		  g.fillOval(startX + ((radius + 7) * j), startY + ((radius + 7) * i), radius, radius);
     	  }
-    	  
-    	  //g.fillOval(startX + (radius), startY + (radius * i), radius, radius);
-    	  //g.fillOval(startX + (2 * radius), startY + (radius * i), radius, radius);
-    	  //g.fillOval(startX + (3 * radius), startY + (radius * i), radius, radius);
-    	  //g.fillOval(startX + (4 * radius), startY + (radius * i), radius, radius);
-    	  //g.fillOval(startX + (5 * radius), startY + (radius * i), radius, radius);
-    	  //g.fillOval(startX + (6 * radius), startY + (radius * i), radius, radius);
       }
-   } // end method paintComponent
-} // end class LinesRectsOvalsJPanel
+   }
+}
